10290896b4245b277e0f8bf223dcca89fa501cb0
Zorbn <public.zorb@gmail.com>
2023-03-13 13:49:13 -0700

    Lower VK version to 1.1

--- src/Vulkan/VKRenderer.cpp
+++ src/Vulkan/VKRenderer.cpp
@@ -713,7 +713,7 @@ void VKRenderer::CreateInstance()
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "PxlRnd";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_2;
+    appInfo.apiVersion = VK_API_VERSION_1_1;
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
